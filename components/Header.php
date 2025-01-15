13ea53c1b2167038c338723284e31bcc5eabe0dd
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2025-01-15 15:34:06 +0100

    Grape
    Grape

--- components/Header.php
+++ components/Header.php
@@ -20,5 +20,4 @@ if(isset($settings)) {
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Cantarell:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-<script src="<?php echo $shop->clean(WEBSITE,'encode');?>assets/js/UX.js"></script>
 <title><?php echo $shop->clean($meta[0]['meta_title'],'encode');?></title>
