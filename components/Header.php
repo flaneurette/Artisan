2be1b59da89c9293136f4c3d9717d912d34c5d5a
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2025-01-15 15:08:52 +0100

    Grape
    Grape

--- components/Header.php
+++ components/Header.php
@@ -1,11 +1,21 @@
+<?php
+if(isset($settings)) {
+	$theme_css = "themes/". $settings[0]['settings.theme'] ."/shop-front.css?rev=1.0";
+	$tokens_css = "themes/". $settings[0]['settings.theme'] ."/tokens.css?rev=1.0";
+	} else {
+	$theme_css = "themes/black/shop-front.css?rev=1.0";
+	$tokens_css = "themes/black/tokens.css?rev=1.0";
+}
+?>
+
 <meta charset="utf-8"/>
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta http-equiv="Content-type" content="text/html; charset=UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="<?php echo $shop->clean($meta[0]['meta_description'],'encode');?>">
 <meta name="keywords" content="<?php echo $shop->clean($meta[0]['meta_tags'],'encode');?>">
-<link rel="stylesheet" href="<?php echo $shop->clean(WEBSITE,'encode');?>assets/css/tokens.css?revision=1.1" />
-<link rel="stylesheet" href="<?php echo $shop->clean(WEBSITE,'encode');?>assets/css/shop-front.css?revision=1.1" />
+<link rel="stylesheet" href="<?php echo $shop->clean(WEBSITE,'encode');?>assets/css/<?php echo $tokens_css;?>" />
+<link rel="stylesheet" href="<?php echo $shop->clean(WEBSITE,'encode');?>assets/css/<?php echo $theme_css;?>" />
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Cantarell:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
--- dashboard/core/ShopSettings.php
+++ dashboard/core/ShopSettings.php
@@ -8,8 +8,8 @@
 			if(isset($_REQUEST['settings_email']) && !empty($_REQUEST['settings_email'])) {
 				$id = 1;
 				$table    = '`shop.settings`';
-				$columns  = ['settings.announcement','settings.email','settings.currency','settings.country.code','settings.free','settings.shipping','settings.mollie.api'];
-				$values   = [$db->clean($_POST['settings_announcement'],'encode'),$db->clean($_POST['settings_email'],'encode'),$db->clean($_POST['settings_currency'],'encode'),$db->clean($_POST['settings_currency_code'],'encode'),$db->clean($_POST['settings_free'],'encode'),$db->clean($_POST['settings_shipping'],'encode'),$db->clean($_POST['settings_mollie_api'],'encode')];
+				$columns  = ['settings.announcement','settings.email','settings.currency','settings.country.code','settings.free','settings.shipping','settings.mollie.api','settings.theme'];
+				$values   = [$db->clean($_POST['settings_announcement'],'encode'),$db->clean($_POST['settings_email'],'encode'),$db->clean($_POST['settings_currency'],'encode'),$db->clean($_POST['settings_currency_code'],'encode'),$db->clean($_POST['settings_free'],'encode'),$db->clean($_POST['settings_shipping'],'encode'),$db->clean($_POST['settings_mollie_api'],'encode'),$db->clean($_POST['settings_theme'],'encode')];
 				$db->update($table,$columns,$values,$id);
 				$success = "Settings successfully changed.";
 			}
@@ -44,6 +44,12 @@
 	?>
 	<input type="hidden" name="csrf" value="<?php echo $token;?>" />
 	<label>Shop Announcement: </label><input type="text" name="settings_announcement" value="<?php echo $db->clean($settings[0]['settings.announcement'],'encode');?>">
+	<label>Theme: </label>
+	<select name="settings_theme" class="select">
+		<option value="white" <?php if($db->clean($settings[0]['settings.theme'],'encode') =='white') { echo "selected"; }?>>White theme design</option>
+		<option value="black" <?php if($db->clean($settings[0]['settings.theme'],'encode') =='black') { echo "selected"; }?>>Black theme design</option>
+	</select>
+	<hr>
 	<label>Shop E-mail: </label><input type="text" name="settings_email" value="<?php echo $db->clean($settings[0]['settings.email'],'encode');?>" required>
 	<!-- <label>Paypal E-mail: </label> <input type="text" name="settings_paypal" value="<?php echo $db->clean($settings[0]['settings.paypal'],'encode');?>" required> -->
 	<label>Mollie API key: </label> <input type="text" name="settings_mollie_api" value="<?php echo $db->clean($settings[0]['settings.mollie.api'],'encode');?>" required>	
